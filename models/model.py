1cc41a01cce87def0cc9b330cc6b31caee996525
thinhcse <111031775+thinhcse@users.noreply.github.com>
2024-01-27 11:53:04 +0700

    add self-attention layer

--- models/model.py
+++ models/model.py
@@ -2,6 +2,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class SelfAttention(nn.Module):
+    def __init__(self, feature_size):
+        super(SelfAttention, self).__init__()
+        self.feature_size = feature_size
+
+        self.key = nn.Linear(feature_size, feature_size)
+        self.query = nn.Linear(feature_size, feature_size)
+        self.value = nn.Linear(feature_size, feature_size)
+    
+    def forward(self, x, mask = None):
+        keys = self.key(x)
+        queries = self.query(x)
+        values = self.value(x)
+
+        scores = torch.matmul(queries, keys.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.feature_size, dtype=torch.float32))
+
+        if mask:
+            scores = scores.masked_fill(mask==0, -1e9)
+        
+        attention_weights = F.softmax(scores, dim = -1)
+        output = torch.matmul(attention_weights, values)
+
+        return output, attention_weights
+    
 class model(nn.Module):
     def __init__(self, configs):
         super().__init__()
@@ -20,7 +44,9 @@ class model(nn.Module):
                             num_layers=configs["model"]["num_lstm_layers"],
                             batch_first=True
                             )
-        self.fc = nn.Linear(configs["model"]["num_hidden_state_features"], 1)
+        self.attention = SelfAttention(feature_size=configs["model"]["num_hidden_state_features"])
+        self.fc1 = nn.Linear(configs["model"]["num_hidden_state_features"], 64)
+        self.fc2 = nn.Linear(64, 1)
     
     def forward(self, input):
         output = input.view((
@@ -36,10 +62,12 @@ class model(nn.Module):
                           0, self.configs["model"]["kernel_size"] - 1 #to capture last row when striding
                       ))
         output = self.conv(output)
-        output = F.relu(output)
+        output = F.avg_pool2d(output, kernel_size=(2, 1))
         output = output.permute((0, 2, 1, -1)) #(batch, window_size, num_out_channel, remain)  
         output = torch.flatten(output, start_dim=2, end_dim=3) #4D -> 3D to be used with lstm
-        output = self.lstm(output)[0] #(h_n, cn)->hn
+        output = self.lstm(output)[0] #(h_n, cn)->hn -> feature_size = feature_size(hn)
+        output = self.attention(output)[0] #values applied attention, attention weight -> take the first one
         output = torch.select(output, dim=1, index=-1) #get the last hidden state (with num_hidden_state_features)
-        output = self.fc(output)
+        output = self.fc1(output)
+        output = self.fc2(output)
         return output
